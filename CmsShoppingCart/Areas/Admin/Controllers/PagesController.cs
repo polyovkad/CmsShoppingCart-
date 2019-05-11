01bcefeed1328c0884cc7ec83a0f8094b5592fb0
polyovkad <polyovkad@gmail.com>
2019-05-11 11:40:31 +0200

    #1 Admin index page added

---
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,29 @@
+﻿using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.Data.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Declare list of PageVM
+            List<PageVM> pageList;
+
+            //Init the list
+            using (Db db = new Db())
+            {
+                //Init the list
+                pageList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+            //Return view with list
+            return View(pageList);
+        }
+    }
+}
